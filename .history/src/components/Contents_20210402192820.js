5056cd458c1fa212f2f86b7e4c2254b3df885235
yuriko-pixel <beljar731@gmail.com>
2021-04-02 19:28:28 +0900

    updated

---
+++ .history/src/components/Contents_20210402192820.js
@@ -0,0 +1,38 @@
+import React, { useEffect,useState } from 'react';
+import css from '../App.css'
+import Moment from 'react-moment';
+
+  export default function Contents() {
+    const [memo, setMemo] = useState([]); 
+
+    const getData = ()=> {
+      fetch('https://yurikomemo.herokuapp.com/api/v1/memo/all', {method:'GET', 
+      headers: {
+        'Authorization': 'Basic ' + btoa('system:password'),
+        'Access-Control-Allow-Origin': '*'
+      },
+      mode: 'cors',
+    credentials: 'include'
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
+
+    useEffect(() => {
+     getData()
+    }, []);
+
+    return(
+        <div className="memoContainer">
+          {memo.filter(item=>item.memoid != null).map(i => <div className="memoparts" key={Math.random()}>
+            <h1  key={Math.random()}>{i.memotitle}</h1>{i.memotitle}
+            <Moment format="YYYY/MM/DD">
+                {i.memodate}
+            </Moment>
+            <div dangerouslySetInnerHTML={{__html: i.memohtml}} className="text" key={Math.random()} ></div>
+                        </div>)
+          }
+        </div>
+    )
+}
+
