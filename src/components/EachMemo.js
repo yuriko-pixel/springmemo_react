093a842fe61ad2946ac363295155afef0a00ab7c
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2021-06-25 15:31:36 +0900

    Update EachMemo.js

--- src/components/EachMemo.js
+++ src/components/EachMemo.js
@@ -7,7 +7,7 @@ const Memo = (props) => {
     const [memo_each,setEachMemo] = useState({});
 
     const get = useCallback(async () => {
-      await fetch(`${process.env.REACT_APP_MEMO_URL}+${props.match.params.memoId}`, {method:'GET', 
+      await fetch(`${process.env.REACT_APP_MEMO_URL}${props.match.params.memoId}`, {method:'GET', 
       headers: {
         'Authorization': 'Basic ' + btoa(process.env.REACT_APP_USERNAME+":"+process.env.REACT_APP_PSW),
         'Access-Control-Allow-Origin': '*'
